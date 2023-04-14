3e3e08e28ec9527f62f5532db77939dff2f5c3fd
Ehfaz Rezwan <ehfaz.rezwan@gmail.com>
2023-04-14 07:33:53 +0600

    Fixed event loop problem with gunicorn

--- api/__init__.py
+++ api/__init__.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI
 
 # from .rate_limit import RateLimiterMiddleware
-from .prompt import router as prompt_router, discord_client
+from .prompt import router as prompt_router, discord_client, start_discord_client
 
 app = FastAPI()
+
+
+@app.on_event("startup")
+async def app_startup():
+    await start_discord_client()
 
 
 # app.add_middleware(RateLimiterMiddleware)
